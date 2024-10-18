8fffc7470d47d46dd93b6fc40dd6ada4d7626c5f
LoneWolf_108 <121552564+Saptarshi-108@users.noreply.github.com>
2024-10-18 23:47:17 +0530

    Add files via upload

---
+++ src/about.js
@@ -0,0 +1,14 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      {/* Your about page content */}
+    </div>
+  );
+};
+
+export default About;
+
---
+++ src/contact.js
@@ -0,0 +1,13 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      {/* Your contact page content */}
+    </div>
+  );
+};
+
+export default Contact;
